0ca6eb1d6b4fe8cd9a205148b31d68a3ceaa57f9
Sara-PzSZ <s.perezsanchez@edu.uah.es>
2025-05-25 19:43:26 +0200

    json

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.conquistapracticafinal {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires com.google.gson;
 
     opens com.example.conquistapracticafinal to javafx.fxml;
     exports com.example.conquistapracticafinal;
