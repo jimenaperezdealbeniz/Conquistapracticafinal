c4cc090b85dd4751ce332c29d39fa4bcc6101065
siracoulon <sira.coulon@edu.uah.es>
2025-05-25 20:08:37 +0200

    PRimer

---
+++ src/PF/Guardar/GestorArchivos.java
@@ -0,0 +1,38 @@
+package PF.Guardar;
+import PF.juego.Partida;
+import PF.jugadores.Jugador;
+import PF.tablero.Tablero;
+import PF.tablero.Casilla;
+import PF.unidades.Unidad;
+
+import com.google.gson.*;
+import java.io.*;
+
+public class GestorArchivos {
+
+    public static void guardarPartida(Partida partida, String rutaArchivo) {
+        try {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            FileWriter writer = new FileWriter(rutaArchivo);
+            gson.toJson(partida, writer);
+            writer.close();
+            System.out.println("Partida guardada en " + rutaArchivo);
+        } catch (IOException e) {
+            System.out.println("Error al guardar la partida: " + e.getMessage());
+        }
+    }
+
+    public static Partida cargarPartida(String rutaArchivo) {
+        try {
+            Gson gson = new Gson();
+            FileReader reader = new FileReader(rutaArchivo);
+            Partida partida = gson.fromJson(reader, Partida.class);
+            reader.close();
+            System.out.println("Partida cargada desde " + rutaArchivo);
+            return partida;
+        } catch (IOException e) {
+            System.out.println("Error al cargar la partida: " + e.getMessage());
+            return null;
+        }
+    }
+}
